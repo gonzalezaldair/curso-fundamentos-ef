0fec0f23c6a104a6f6923a0ba20ba4f90b12c823
Aldair Alfonso Gonzalez Amaya <aldair.gonzalez@unipamplona.edu.co>
2023-04-12 12:53:17 -0500

    Clase 24 -> Eliminando datos con Entity framework

--- Program.cs
+++ Program.cs
@@ -16,21 +16,21 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 
-app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
 
-    return Results.Ok("Base da Datos en Memoria : "+ dbContext.Database.IsInMemory());
+    return Results.Ok("Base da Datos en Memoria : " + dbContext.Database.IsInMemory());
 
 
 });
 
-app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) => 
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
 {
     return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
 });
 
-app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) => 
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
 {
     tarea.TareaId = Guid.NewGuid();
     tarea.FechaCreacion = DateTime.Now;
@@ -43,14 +43,11 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
 });
 
 
-app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) => 
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
 {
-
-
     var tareaActual = dbContext.Tareas.Find(id);
 
-
-    if(tareaActual != null)
+    if (tareaActual != null)
     {
         tareaActual.CategoriaId = tarea.CategoriaId;
         tareaActual.Titulo = tarea.Titulo;
@@ -59,10 +56,28 @@ app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [F
 
         await dbContext.SaveChangesAsync();
 
-         return Results.Ok();
+        return Results.Ok();
     }
 
-     return Results.NotFound();
+    return Results.NotFound();
+});
+
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tareaActual != null)
+    {
+        dbContext.Remove(tareaActual);
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
 });
 
 
